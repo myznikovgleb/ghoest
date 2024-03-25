cef094ffa8733fbc8e4ef182dc1d1636628cf527
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-03-25 21:27:43 +0300

    Add debug mode

--- src/pages/_/_.tsx
+++ src/pages/_/_.tsx
@@ -1,10 +1,29 @@
+import { useCallback, useEffect, useState } from 'react'
+
 import { Layout } from '@/shared/ui'
 import { Experience } from '@/widgets/experience'
 
+const DEBUG_MARKER_HASH_PROPERTY = '#debug'
+
 const _ = () => {
+  const [isDebug, setIsDebug] = useState<boolean>(
+    window.location.hash === DEBUG_MARKER_HASH_PROPERTY
+  )
+
+  const handleHashChange = useCallback(() => {
+    setIsDebug(window.location.hash === DEBUG_MARKER_HASH_PROPERTY)
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('hashchange', handleHashChange)
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange)
+    }
+  }, [handleHashChange])
+
   return (
     <Layout>
-      <Experience />
+      <Experience isDebug={isDebug} />
     </Layout>
   )
 }
--- src/widgets/experience/experience.tsx
+++ src/widgets/experience/experience.tsx
@@ -4,14 +4,20 @@ import { Physics } from '@react-three/rapier'
 
 import { Map, Player } from '@/entities'
 
-const Experience = () => {
+interface ExperienceProps {
+  isDebug: boolean
+}
+
+const Experience = (props: ExperienceProps) => {
+  const { isDebug } = props
+
   return (
     <Canvas camera={{ position: [0, 2, -4] }} className="cursor-pointer">
       <Environment preset="city" />
 
-      <OrbitControls />
+      {isDebug && <OrbitControls />}
 
-      <Physics>
+      <Physics debug={isDebug}>
         <Player />
         <Map />
       </Physics>
