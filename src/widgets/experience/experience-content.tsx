be09857cfc4c3fd611c943f9a8a34e9ae853115b
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-06-12 22:48:48 +0300

    Update debug panel

--- src/widgets/experience/experience-content.tsx
+++ src/widgets/experience/experience-content.tsx
@@ -1,11 +1,16 @@
 import { OrbitControls } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
 import { Physics } from '@react-three/rapier'
-import { useEffect } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Vector3 } from 'three'
 
 import { Map } from '@/entities/map'
 import { Player } from '@/entities/player'
+
+enum ExperienceState {
+  Pending,
+  Mounted,
+}
 
 interface ExperienceContentProps {
   isEnabledControls?: boolean
@@ -20,26 +25,63 @@ const ExperienceContent = (props: ExperienceContentProps) => {
     isStickedCamera = false,
   } = props
 
-  const { camera } = useThree()
+  const camera = useThree((state) => state.camera)
+
+  const [experienceState, setExperienceState] = useState<ExperienceState>(
+    ExperienceState.Pending
+  )
 
   useEffect(() => {
-    if (isStickedCamera) {
+    switch (experienceState) {
+      case ExperienceState.Pending: {
+        setExperienceState(ExperienceState.Mounted)
+        break
+      }
+      case ExperienceState.Mounted:
+      default: {
+        break
+      }
+    }
+  }, [experienceState])
+
+  useEffect(() => {
+    const stickCamera = () => {
       const cameraPositionDebug = new Vector3(0, 4, 4)
       const cameraTargetDebug = new Vector3(0, 0, 0)
 
       camera.position.copy(cameraPositionDebug)
       camera.lookAt(cameraTargetDebug)
     }
-  }, [camera, isStickedCamera])
+
+    switch (experienceState) {
+      case ExperienceState.Pending: {
+        stickCamera()
+
+        break
+      }
+      case ExperienceState.Mounted:
+      default: {
+        if (!isStickedCamera) {
+          break
+        }
+
+        stickCamera()
+
+        break
+      }
+    }
+  }, [experienceState, isStickedCamera, camera])
 
   return (
     <>
       {isEnabledControls && <OrbitControls />}
 
-      <Physics debug={isDebugPhysics}>
-        <Player isStickedCamera={isStickedCamera} />
-        <Map />
-      </Physics>
+      <Suspense>
+        <Physics debug={isDebugPhysics}>
+          <Player isStickedCamera={isStickedCamera} />
+          <Map />
+        </Physics>
+      </Suspense>
     </>
   )
 }
