3774d382ecfec6b42dc71730191b967d409b6b83
Gleb Myznikov <myznikovgleb@yandex.ru>
2025-03-30 20:20:08 +0300

    Update assets

---
+++ src/shared/resources/key/key.tsx
@@ -0,0 +1,40 @@
+import { useGLTF, useTexture } from '@react-three/drei'
+import { forwardRef } from 'react'
+
+import type { Group, Mesh, Vector3Tuple } from 'three'
+import type { GLTF } from 'three-stdlib'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    key_bar: Mesh
+    key_bit: Mesh
+  }
+  materials: Record<string, never>
+}
+
+interface KeyProps {
+  position?: Vector3Tuple
+  rotation?: Vector3Tuple
+}
+
+const Key = forwardRef<Group, KeyProps>((props, ref) => {
+  const { nodes } = useGLTF('./experience/key.glb') as GLTFResult
+
+  const matcapGold = useTexture('./experience/matcap_gold.png')
+
+  return (
+    <group ref={ref} dispose={null} {...props}>
+      <mesh geometry={nodes.key_bar.geometry}>
+        <meshMatcapMaterial matcap={matcapGold} />
+      </mesh>
+      <mesh geometry={nodes.key_bit.geometry}>
+        <meshMatcapMaterial matcap={matcapGold} />
+      </mesh>
+    </group>
+  )
+})
+
+useGLTF.preload('./experience/key.glb')
+useTexture.preload('./experience/matcap_gold.png')
+
+export { Key }
